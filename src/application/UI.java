362ca33ba8eebd2c02b443827890c22249d5a33e
“a-vecchi” <alexandre_v@yahoo.com>
2021-06-03 17:20:14 -0300

    Imprimindo/Mostrando possíveis movimentos de uma peça

--- src/application/UI.java
+++ src/application/UI.java
@@ -42,7 +42,8 @@ public class UI {
 			int row = Integer.parseInt(s.substring(1));
 			return new ChessPosition(column, row);
 		} catch (RuntimeException e) {
-			throw new InputMismatchException("Posição no tabuleiro de xadrez não é válida. Valores válidos de a1 até h8.");
+			throw new InputMismatchException(
+					"Posição no tabuleiro de xadrez não é válida. Valores válidos de a1 até h8.");
 		}
 	}
 
@@ -50,16 +51,30 @@ public class UI {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
 			for (int j = 0; j < pieces.length; j++) {
-				printPiece(pieces[i][j]);
+				printPiece(pieces[i][j], false);
 			}
 			System.out.println();
 		}
 		System.out.println("  a b c d e f g h");
 	}
 
-	private static void printPiece(ChessPiece piece) {
+	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j = 0; j < pieces.length; j++) {
+				printPiece(pieces[i][j], possibleMoves[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println("  a b c d e f g h");
+	}
+
+	private static void printPiece(ChessPiece piece, boolean backGround) {
+		if (backGround) {
+			System.out.print(ANSI_BLUE_BACKGROUND);
+		}
 		if (piece == null) {
-			System.out.print("-");
+			System.out.print("-" + ANSI_RESET);
 		} else {
 			if (piece.getColor() == Color.WHITE) {
 				System.out.print(ANSI_WHITE + piece + ANSI_RESET);
